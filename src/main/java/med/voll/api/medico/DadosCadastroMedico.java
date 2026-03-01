2ee6b35acb87fc8eec00ea45014be49cfea7fa34
Carlos Vieira <c.gleysonti@gmail.com>
2026-02-28 22:57:16 -0300

    Criando as validações com Bean Validation

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,9 +1,29 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email,
-                                  String crm,
-                                  Especialidade especialidade,
-                                  DadosEndereco endereco) {
+public record DadosCadastroMedico(
+
+        @NotBlank //valida se uma String não é nula e se, após remover os espaços em branco do início e do fim (trim), seu tamanho é maior que zero
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")//indica quantidade mínima e máxima
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
 }
