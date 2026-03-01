9c23336c228c54899e535e7b35dde32d7117753f
Carlos Vieira <c.gleysonti@gmail.com>
2026-03-01 19:14:56 -0300

    deletar medico por id

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -40,4 +40,10 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
