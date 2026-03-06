73b8b37845f3047f64d723522eea3dbd5d2e2351
Carlos Vieira <c.gleysonti@gmail.com>
2026-03-05 22:13:11 -0300

    Validação de token e auteticação atravez do token gerado

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,22 +16,68 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//    @Autowired
+//    private TokenService tokenService;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        var tokenJWT = recuperarToken(request);
+//
+//        filterChain.doFilter(request, response);
+//
+//        var subject = tokenService.getSubject(tokenJWT);
+//        System.out.println(subject);
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//
+//    private String recuperarToken(HttpServletRequest request) {
+//        var authorizationHeader = request.getHeader("Authorization");
+//
+//        if (authorizationHeader == null){
+//            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+//        }
+//        return authorizationHeader.replace("Bearer ", "");
+//    }
 
-        //System.out.println("FILTRO CHAMADO");
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
         var tokenJWT = recuperarToken(request);
 
-        System.out.println(tokenJWT);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            //System.out.println(subject);
+        }
+
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null){
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+            //return null;
         }
-        return authorizationHeader.replace("Bearer", "");
+       //throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        return null;
+
     }
 }
