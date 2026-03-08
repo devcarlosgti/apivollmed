0ee7343771c68856f0689ef2f0e89eeac0187c3b
Carlos Vieira <c.gleysonti@gmail.com>
2026-03-08 01:17:45 -0300

    Conclusão de testes automatizados

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 
 public interface MedicoRepository extends JpaRepository<Medico,Long> {
+
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
     @Query("""
